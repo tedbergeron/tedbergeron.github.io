856cfa52650bc1dc26f667b48d4e6841bcddf13d
tedbergeron <tbergeron@cisoregon.org>
2015-03-05 13:27:59 -0800

    add new

--- jekyll/zCopyToUpload.bat
+++
@@ -1,8 +0,0 @@
-REM copy Jekyll rendered pages in _site to my github.io repo
-
-
-Xcopy /S /E /I /Y C:\_dev\jekyll\_site C:\_dev\tedbergeron.github.io
-
-
-REM ### File copy has completed ###
-PAUSE
